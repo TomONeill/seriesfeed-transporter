21a762a5c0680067c16c7480bca4e2ed6eef5cf6
Tom O'Neill <TomONeill@users.noreply.github.com>
2020-02-23 00:55:47 +0100

    Added type to NavigationService

--- src/Navigation/Services/NavigationService.ts
+++ src/Navigation/Services/NavigationService.ts
@@ -3,7 +3,7 @@
 module SeriesfeedTransporter.Services {
     export class NavigationService {
         public static add(navigationType: Enums.NavigationType, position: number, text: string, url: string): void {
-            const mainMenuItem = document.querySelector("ul.main-menu .submenu .inner .top-level:nth-child(" + navigationType + ")");
+            const mainMenuItem = document.querySelector<HTMLLIElement>("ul.main-menu .submenu .inner li.top-level:nth-child(" + navigationType + ")");
             const menuItem = ElementService.createElement<HTMLLIElement>("<li><a href='" + url + "'>" + text + "</a></li>");
             mainMenuItem.querySelector(".main-menu-dropdown li:nth-child(" + position + ")").before(menuItem);
         }
