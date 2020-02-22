5a29796c0c1a519637b43fd5807ca64b8d8ca02a
Tom O'Neill <TomONeill@users.noreply.github.com>
2020-02-23 00:30:03 +0100

    Refactored SettingsController

--- src/Settings/SettingsController.ts
+++ src/Settings/SettingsController.ts
@@ -7,37 +7,38 @@ module SeriesfeedTransporter.Controllers {
                 return;
             }
 
-            const settingBlocks = $('.container.content .row');
-
+            const settingBlocks = document.querySelector(".container.content .row");
             const localStorageBlock = this.getLocalStorageBlock();
-
             settingBlocks.append(localStorageBlock);
 
             // TODO: Sync Bierdopje to Seriesfeed here?
         }
 
-        private getLocalStorageBlock(): JQuery<HTMLElement> {
-            const block = $('<div/>').addClass('col-xs-12 col-md-6');
-            const card = $('<div/>').attr('id', 'userscriptTool').addClass('blog-left cardStyle cardForm');
-            const cardContent = $('<div/>').addClass('blog-content');
-            const cardTitle = $('<h3/>').text("Userscript Seriesfeed Transporter");
-            const cardParagraph = $('<p/>').text("Dit script slaat gegevens van series en afleveringen op om de druk op de gerelateerde servers te verlagen. Deze data wordt gebruikt om (bij terugkerende acties) bekende data niet opnieuw te hoeven ophalen. Je kunt de lokale gegevens wissen als je problemen ondervindt met importeren/exporteren.");
-            const dataDeleted = $('<p/>').text("De gegevens zijn gewist.").css({ marginBottom: '0', paddingTop: '5px' }).hide();
-            const buttonAction = () => {
-                dataDeleted.hide();
+        private getLocalStorageBlock(): HTMLDivElement {
+            const localStorageBlockHtml = `
+            <div class="col-xs-12 col-md-6">
+                <div id="userscriptTool" class="blog-left cardStyle cardForm">
+                    <div class="blog-content">
+                        <h3>Userscript Seriesfeed Transporter</h3>
+                        <p>Dit script slaat gegevens van series en afleveringen op om de druk op de gerelateerde servers te verlagen. Deze data wordt gebruikt om (bij terugkerende acties) bekende data niet opnieuw te hoeven ophalen. Je kunt de lokale gegevens wissen als je problemen ondervindt met importeren/exporteren.</p>
+                        <fieldset class="fieldset-save">
+                            <input type="button" class="btn btn-success delete btn-right" value="Lokale gegevens wissen" sft-handle="btn-delete" />
+                        </fieldset>
+                        <span sft-handle="msg-deleted" hidden="true" style="margin-bottom: 0px; padding-top: 5px;">De gegevens zijn gewist.</span>
+                    </div>
+                </div>
+            </div>
+            `;
+            const localStorageBlock = Services.ElementService.createElement<HTMLDivElement>(localStorageBlockHtml);
+            const deleteButton = localStorageBlock.querySelector<HTMLInputElement>("[sft-handle=btn-delete]");
+            const deletedMessage = localStorageBlock.querySelector<HTMLSpanElement>("[sft-handle=msg-deleted]");
+            deleteButton.onclick = () => {
+                deletedMessage.hidden = true;
                 Services.StorageService.clearAll();
-                setTimeout(() => dataDeleted.show(), 100);
+                setTimeout(() => deletedMessage.hidden = false, 100);
             };
-            const button = new ViewModels.Button('btn-success', 'fa-trash', "Lokale gegevens wissen", buttonAction);
 
-            block.append(card);
-            card.append(cardContent);
-            cardContent.append(cardTitle);
-            cardContent.append(cardParagraph);
-            cardContent.append(button.instance);
-            cardContent.append(dataDeleted);
-
-            return block;
+            return localStorageBlock;
         }
     }
 }
