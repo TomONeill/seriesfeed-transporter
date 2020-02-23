768face05ef4078c39284e6029637dcf1c33dc0f
Tom O'Neill <TomONeill@users.noreply.github.com>
2020-02-23 14:11:26 +0100

    Fix nullpointer in NavigationService

--- src/Navigation/Services/NavigationService.ts
+++ src/Navigation/Services/NavigationService.ts
@@ -5,7 +5,7 @@ module SeriesfeedTransporter.Services {
         public static add(navigationType: Enums.NavigationType, position: number, text: string, url: string): void {
             const mainMenuItem = document.querySelector<HTMLLIElement>("ul.main-menu .submenu .inner li.top-level:nth-child(" + navigationType + ")");
             const menuItem = ElementService.createElement<HTMLLIElement>("<li><a href='" + url + "'>" + text + "</a></li>");
-            mainMenuItem.querySelector(".main-menu-dropdown li:nth-child(" + position + ")").before(menuItem);
+            mainMenuItem.querySelector(".main-menu-dropdown li:nth-child(" + position + ")")?.before(menuItem);
         }
     }
 }
